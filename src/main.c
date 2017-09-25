f56be777853364430b36ac9d865e2f67b5829a55
mara.kovalcik@gmail.com <mara.kovalcik@gmail.com>
2017-09-25 20:55:50 +0200

    Nahrávání souboru z stdin počet argumentů > 1 == printHelp() ./proj <<< parser.c --- vypíše obsah souboru na stdout

--- src/main.c
+++ src/main.c
@@ -12,28 +12,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //#include "symtable.h"
 #include "error_code.h"
 
 ERROR_CODE code_error = ERROR_CODE_OK;
-FILE *pFile;
 
-int main(int argc, char **argv) {
+void printHelp();
 
-    // kontrola správného počtu argumentů
-    if(argc != 2){
-        fprintf(stderr, "Nespravny pocet argumentu. ERROR CODE 99\n");
+int main(int argc, char **argv)
+{
+    FILE *pFile;
+    char filename[200];
+    char *newline;
+    (void)argv;
+
+    if(argc != 1) {
+        printHelp();
+        return code_error;
+    }
+
+    if (fgets(filename, sizeof(filename), stdin) == NULL) {
+        fprintf(stderr, "Input error: ERROR CODE 99\n");
         return code_error = ERROR_CODE_INTERNAL;
     }
 
-    // otevření souboru pro čtení
-    if((pFile = fopen(argv[1], "r")) == NULL){
-        fprintf(stderr,"Soubor '%s' se nepodarilo otevrit. ERROR CODE 99\n",argv[1]);
-        return code_error = ERROR_CODE_INTERNAL;
-    }
+    newline = strchr(filename, '\n');
+    if (newline)
+        *newline = '\0';
 
-    fprintf(stdout,"Soubor '%s' byl otevren a nasledne zavren. ERROR CODE 0\n", argv[1]);
+    if ((pFile = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "Soubor '%s' se nepodarilo otevrit: ERROR CODE 99\n", filename);
+        return code_error = ERROR_CODE_INTERNAL;
+    } else {
+        int chr;
+        while ((chr = fgetc(pFile)) != EOF)
+            fputc(chr, stdout);
+    }
+    printf("\n");
+    printf("sadfasdfasdfasdfasfasdf\n");
 
     fclose(pFile);
     return code_error;
+}
+
+void printHelp(){
+    printf("help help help help\n");
 }
