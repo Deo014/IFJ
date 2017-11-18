763889f6232940d4c2a8849ff30e79021af7a73f
Ondřej Valušek <ondrej.valusek@seznam.cz>
2017-11-18 14:32:10 +0100

    Doplneny komentare s pravidly gramatiky

--- src/parser.c
+++ src/parser.c
@@ -94,6 +94,7 @@ int parse(tSymtable *symtable, tDLListInstruction *instrList) {
 }
 
 int Program() {
+    //<Program> -> <Deklarace_funkci_definice_funkci><Telo_programu><EOF>
     int result;
     switch (aktualni_token.type) {
         case sDeclare:
@@ -115,18 +116,21 @@ int Program() {
 int Deklarace_fci_definice_fci() {
     int result;
     switch (aktualni_token.type) {
+        //<Deklarace_funkci_definice_funkci> -> < Nekolik_Deklaraci_fce > <Deklarace_funkci_definice_funkci>
         case sDeclare:
             result = Nekolik_deklaraci_fce();
             if (result != ERROR_CODE_OK) return result;
             result = Deklarace_fci_definice_fci();
             if (result != ERROR_CODE_OK) return result;
             return ERROR_CODE_OK;
+            //<Deklarace_funkci_definice_funkci> -> < Nekolik_Definici_fce > <Deklarace_funkci_definice_funkci>
         case sFuntion:
             result = Nekolik_definici_fce();
             if (result != ERROR_CODE_OK) return result;
             result = Deklarace_fci_definice_fci();
             if (result != ERROR_CODE_OK) return result;
             return ERROR_CODE_OK;
+            //<Deklarace_funkci_definice_funkci> -> e
         case sScope:
             return ERROR_CODE_OK;
 
@@ -136,18 +140,16 @@ int Deklarace_fci_definice_fci() {
 
 int Nekolik_deklaraci_fce() {
     int result;
-    switch (aktualni_token.type)
-    {
+    switch (aktualni_token.type) {
+        //<Nekolik_Deklaraci_fce> -> <Deklarace_fce> <Nekolik_Deklaraci_fce>
         case sDeclare:
             result = Deklarace_fce();
             if (result != ERROR_CODE_OK) return result;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             return Nekolik_deklaraci_fce();
+//<Nekolik_Deklaraci_fce> -> e
         case sFuntion:
-            result = Nekolik_definici_fce();
-            if (result != ERROR_CODE_OK) return result;
         case sScope:
-
             return ERROR_CODE_OK;
     }
 
@@ -156,13 +158,15 @@ int Nekolik_deklaraci_fce() {
 
 int Nekolik_definici_fce() {
     int result;
-    switch (aktualni_token.type)
-    {
+    switch (aktualni_token.type) {
+        //<Nekolik_Definici_fce> -> < Definice_fce><Nekolik_Definici_fce>
         case sFuntion:
             result = Definice_fce();
             if (result != ERROR_CODE_OK) return result;
             return Nekolik_definici_fce();
+            //<Nekolik_Definici_fce> -> e
         case sDeclare:
+        case sScope:
             return ERROR_CODE_OK;
     }
 
@@ -170,10 +174,9 @@ int Nekolik_definici_fce() {
 }
 
 int Telo_programu() {
+    //<Telo_programu> -> <Scope><Deklarace_promennych_a _prikazy><End><Scope>
     int result;
-
-    switch (aktualni_token.type)
-    {
+    switch (aktualni_token.type) {
         case sScope:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (aktualni_token.type != sEndOfLine) return ERROR_CODE_SYN;
@@ -190,8 +193,8 @@ int Telo_programu() {
 }
 
 int Deklarace_fce() {
+    //<Deklarace_fce> -> <Declare> <Hlavicka_fce><EOL>
     int result;
-
     switch (aktualni_token.type) {
         case sDeclare:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
@@ -206,9 +209,9 @@ int Deklarace_fce() {
 }
 
 int Definice_fce() {
+    //<Definice_fce> -> <Hlavicka_fce><EOL><Telo_fce><End><Function><EOL>
     int result;
     switch (aktualni_token.type) {
-
         case sFuntion:
             result = Hlavicka_fce();
             if (result != ERROR_CODE_OK) return result;
@@ -228,6 +231,7 @@ int Definice_fce() {
 }
 
 int Hlavicka_fce() {
+    //<Hlavicka_fce> -> <Function><Id><<(><Parametry><)><As><Typ>
     int result;
     if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
     if (aktualni_token.type != sIdentificator) return ERROR_CODE_SYN;
@@ -246,15 +250,22 @@ int Hlavicka_fce() {
 }
 
 int Typ() {
-    if (aktualni_token.type == tInteger || aktualni_token.type == tDouble || aktualni_token.type == tString)
-        return ERROR_CODE_OK;
-    else
+    switch (aktualni_token.type) {
+        //<Typ> -> <Integer>
+        //<Typ> -> <Double>
+        //<Typ> -> <String>
+        case tInteger:
+        case tDouble:
+        case tString:
+            return ERROR_CODE_OK;
+    }
         return ERROR_CODE_SYN;
 }
 
 int Parametry() {
     int result;
     switch (aktualni_token.type) {
+        //<Parametry> -> <Id><As><Typ><Dalsi_parametry>
         case sIdentificator:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (aktualni_token.type != sAs) return ERROR_CODE_SYN;
@@ -265,6 +276,7 @@ int Parametry() {
             result = Dalsi_parametry();
             if (result != ERROR_CODE_OK) return result;
             return ERROR_CODE_OK;
+            //<Parametry> -> e
         case sRightPar:
             return ERROR_CODE_OK;
 
@@ -275,11 +287,13 @@ int Parametry() {
 int Dalsi_parametry() {
     int result;
     switch (aktualni_token.type) {
+        //<Dalsi_parametry> -> <,><Parametry><Dalsi_parametry>
         case sComma:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             result = Parametry();
             if (result != ERROR_CODE_OK) return result;
             return Dalsi_parametry();
+            //<Dalsi_parametry> -> e
         case sRightPar:
             return ERROR_CODE_OK;
     }
@@ -287,21 +301,23 @@ int Dalsi_parametry() {
 }
 
 int Telo_funkce() {
+    //<Telo_fce> -> <Deklarace_promennych_a _prikazy>
     int result;
     if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
     result = Deklarace_prom_a_prikazy();
-            if (result != ERROR_CODE_OK) return result;
-            return ERROR_CODE_OK;
+    if (result != ERROR_CODE_OK) return result;
+    return ERROR_CODE_OK;
 }
 
 int Deklarace_promennych() {
     int result;
-    switch (aktualni_token.type)
-    {
+    switch (aktualni_token.type) {
+        //< Deklarace _promennych> ->< Deklarace_promenne> <Deklarace_promennych>
         case sDim:
             result = Deklarace_promenne();
             if (result != ERROR_CODE_OK) return result;
             return Deklarace_promennych();
+            //< Deklarace _promennych> -> e
         case sScope:
         case sEnd:
         case sIdentificator:
@@ -319,18 +335,18 @@ int Deklarace_promennych() {
 
 int Prikazy() {
     int result;
-    switch (aktualni_token.type)
-    {
+    switch (aktualni_token.type) {
+        //<Prikazy> -> <Prikaz><Prikazy>
         case sPrint:
         case sInput:
         case sIf:
         case sDo:
         case sIdentificator:
         case sReturn:
-
             result = Prikaz();
             if (result != ERROR_CODE_OK) return result;
             return Prikazy();
+            //<Prikazy> -> e
         case sElse:
         case sLoop:
         case sEnd:
@@ -345,6 +361,7 @@ int Prikazy() {
 int Prikaz() {
     int result = 0;
     switch (aktualni_token.type) {
+        //<Prikaz> -> <Print><Vyraz><;><Dalsi_vyrazy><EOL>
         case sPrint:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             result = Vyraz();
@@ -355,6 +372,7 @@ int Prikaz() {
             if (aktualni_token.type != sEndOfLine) return ERROR_CODE_SYN;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             break;
+            //<Prikaz> -> <Input><Id><EOL>
         case sInput:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (aktualni_token.type != sIdentificator) return ERROR_CODE_SYN;
@@ -362,6 +380,7 @@ int Prikaz() {
             if (aktualni_token.type != sEndOfLine) return ERROR_CODE_SYN;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             break;
+            //<Prikaz> -> <If><Vyraz><Then><EOL><Prikazy><Else><EOL><Prikazy><End><If><EOL>
         case sIf:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             result = Vyraz();
@@ -385,6 +404,7 @@ int Prikaz() {
             if (aktualni_token.type != sEndOfLine) return ERROR_CODE_SYN;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             break;
+            //<Prikaz> -> <Do><While><Vyraz><EOL><Prikazy><Loop><EOL>
         case sDo:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (aktualni_token.type != sWhile) return ERROR_CODE_SYN;
@@ -400,6 +420,7 @@ int Prikaz() {
             if (aktualni_token.type != sEndOfLine) return ERROR_CODE_SYN;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             break;
+            //<Prikaz> -> <Id><=><Vyraz><EOL>
         case sIdentificator:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (aktualni_token.type != sAssignment) return ERROR_CODE_SYN;
@@ -409,6 +430,7 @@ int Prikaz() {
             if (aktualni_token.type != sEndOfLine) return ERROR_CODE_SYN;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             break;
+            //<Prikaz> -> <Return><Vyraz><EOL>
         case sReturn:
             //TODO Semantikou vyresit aby nemohl byt v hlavnim tele
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
@@ -423,9 +445,9 @@ int Prikaz() {
 }
 
 int Deklarace_promenne() {
+    //<Deklarace_promenne> -> <Dim><Id><As><Typ><Prirazeni_hodnoty>
     int result;
-    switch (aktualni_token.type)
-    {
+    switch (aktualni_token.type) {
         case sDim:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (aktualni_token.type != sIdentificator) return ERROR_CODE_SYN;
@@ -435,8 +457,7 @@ int Deklarace_promenne() {
             result = Typ();
             if (result != ERROR_CODE_OK) return result;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
-            switch (aktualni_token.type)
-            {
+            switch (aktualni_token.type) {
                 case sAssignment:
                     result = Prirazeni_hodnoty();
                     if (result != ERROR_CODE_OK) return result;
@@ -454,6 +475,7 @@ int Deklarace_promenne() {
 int Deklarace_prom_a_prikazy() {
     int result;
     switch (aktualni_token.type) {
+        //< Deklarace_promennych_a _prikazy >-> <Prikazy><Deklarace_promennych_a _prikazy>
         case sPrint:
         case sInput:
         case sIf:
@@ -465,12 +487,14 @@ int Deklarace_prom_a_prikazy() {
             result = Deklarace_prom_a_prikazy();
             if (result != ERROR_CODE_OK) return result;
             return ERROR_CODE_OK;
+            //< Deklarace_promennych_a _prikazy> -> <Deklarace_promennych> <Deklarace_promennych_a _prikazy>
         case sDim:
             result = Deklarace_promennych();
             if (result != ERROR_CODE_OK) return result;
             result = Deklarace_prom_a_prikazy();
             if (result != ERROR_CODE_OK) return result;
             return ERROR_CODE_OK;
+            //<Deklarace_promennych_a _prikazy> -> e
         case sEnd:
         case sScope:
             return ERROR_CODE_OK;
@@ -482,11 +506,13 @@ int Deklarace_prom_a_prikazy() {
 int Prirazeni_hodnoty() {
     int result;
     switch (aktualni_token.type) {
+        //<Prirazeni_hodnoty> -><=><Vyraz>
         case sAssignment:
             result = Vyraz();
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             return result;
+            //<Prirazeni_hodnoty> -> e
         case sEndOfLine:
             if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
             return ERROR_CODE_OK;
@@ -500,6 +526,7 @@ int Dalsi_vyrazy() {
     int result;
     if (dalsiToken() != ERROR_CODE_OK) return ERROR_CODE_LEX;
     switch (aktualni_token.type) {
+        //<Dalsi_vyrazy> -> <Vyraz> <;><Dalsi_vyrazy>
         case sIdentificator:
         case sInteger:
         case sDouble:
@@ -509,8 +536,8 @@ int Dalsi_vyrazy() {
             if (aktualni_token.type != sSemicolon) return ERROR_CODE_SYN;
             result = Dalsi_vyrazy();
             if (result != ERROR_CODE_OK) return result;
+            //<Dalsi_vyrazy> -> e
         case sEndOfLine:
-        case sEnd:
             return ERROR_CODE_OK;
     }
     return ERROR_CODE_SYN;
