134eb1ceb1cdf4ff5bbc4cf644ba37021e872f42
davidhribek <scottik4@seznam.cz>
2017-11-22 11:38:44 +0100

    scanner: vypocet faktorialu rekurzivne

--- tests/scanner/scanner_tests.c
+++ tests/scanner/scanner_tests.c
@@ -304,12 +304,613 @@ static void test2(void** state) {
 
 }
 
+static void test3(void** state) {
+    (void) state; // unused
+    freopen("test3.txt", "r", stdin);
+
+    tToken token;
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "declare");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "function");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "factorial");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sLeftPar);
+    assert_string_equal(token.atr.value, "(");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sRightPar);
+    assert_string_equal(token.atr.value, ")");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "function");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "factorial");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sLeftPar);
+    assert_string_equal(token.atr.value, "(");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sRightPar);
+    assert_string_equal(token.atr.value, ")");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "dim");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "temp_result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "dim");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "decremented_n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "dim");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "if");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sLess);
+    assert_string_equal(token.atr.value, "<");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sInteger);
+    assert_string_equal(token.atr.value, "2");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "then");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sAssignment);
+    assert_string_equal(token.atr.value, "=");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sInteger);
+    assert_string_equal(token.atr.value, "1");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "else");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "decremented_n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sAssignment);
+    assert_string_equal(token.atr.value, "=");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sMinus);
+    assert_string_equal(token.atr.value, "-");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sInteger);
+    assert_string_equal(token.atr.value, "1");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "temp_result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sAssignment);
+    assert_string_equal(token.atr.value, "=");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "factorial");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sLeftPar);
+    assert_string_equal(token.atr.value, "(");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "decremented_n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sRightPar);
+    assert_string_equal(token.atr.value, ")");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sAssignment);
+    assert_string_equal(token.atr.value, "=");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "n");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sMultiply);
+    assert_string_equal(token.atr.value, "*");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "temp_result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "end");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "if");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "return");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "result");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "end");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "function");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "scope");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "dim");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "a");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "dim");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "vysl");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "as");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "integer");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "print");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sString);
+    assert_string_equal(token.atr.value, "Zadejte\\032cislo\\032pro\\032vypocet\\032faktorialu");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sSemicolon);
+    assert_string_equal(token.atr.value, ";");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "input");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "a");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "if");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "a");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sLess);
+    assert_string_equal(token.atr.value, "<");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sInteger);
+    assert_string_equal(token.atr.value, "0");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "then");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "print");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sString);
+    assert_string_equal(token.atr.value, "\\010Faktorial\\032nelze\\032spocitat\\010");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sSemicolon);
+    assert_string_equal(token.atr.value, ";");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "else");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "vysl");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sAssignment);
+    assert_string_equal(token.atr.value, "=");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "factorial");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sLeftPar);
+    assert_string_equal(token.atr.value, "(");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "a");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sRightPar);
+    assert_string_equal(token.atr.value, ")");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "print");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sString);
+    assert_string_equal(token.atr.value, "\\010Vysledek\\032je:");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sSemicolon);
+    assert_string_equal(token.atr.value, ";");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sIdentificator);
+    assert_string_equal(token.atr.value, "vysl");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sSemicolon);
+    assert_string_equal(token.atr.value, ";");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sString);
+    assert_string_equal(token.atr.value, "\\010");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sSemicolon);
+    assert_string_equal(token.atr.value, ";");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "end");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "if");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    /* next line */
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "end");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sKeyWord);
+    assert_string_equal(token.atr.value, "scope");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfLine);
+    assert_string_equal(token.atr.value, "EOL");
+
+    token = getNextToken();
+    assert_int_equal(token.type, sEndOfFile);
+    assert_string_equal(token.atr.value, "EOF");
+
+
+}
+
 int main(void)
 {
     const struct CMUnitTest tests[] =
             {
                     cmocka_unit_test(test1),
                     cmocka_unit_test(test2),
+                    cmocka_unit_test(test3),
             };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
