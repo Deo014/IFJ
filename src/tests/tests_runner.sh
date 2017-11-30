d04dcb66e247706bdbed8df94bd3a45b8eb2016b
davidhribek <scottik4@seznam.cz>
2017-11-30 21:18:30 +0100

    testy interpreter

--- src/tests/tests_runner.sh
+++ src/tests/tests_runner.sh
@@ -2,37 +2,51 @@
 
 proj=./proj
 interpreter=./ic17int
-test_output_file_path=./tests/tmp_test_output_file_ifjcode17
+test_output_file=./tests/tmp_file
 
-inputs_succ_error0=./tests/vstupy/succ_error0/*
-inputs_lexx_error1=./tests/vstupy/lexx_error1/*
-inputs_synt_error2=./tests/vstupy/synt_error2/*
-inputs_semm_error3=./tests/vstupy/semm_error3/*
-inputs_semm_error4=./tests/vstupy/semm_error4/*
+inputs_succ_error0=./tests/vstupy/succ_error0/*.input
+outputs_succ_error0_path=./tests/vstupy/succ_error0
+inputs_lexx_error1=./tests/vstupy/lexx_error1/*.input
+inputs_synt_error2=./tests/vstupy/synt_error2/*.input
+inputs_semm_error3=./tests/vstupy/semm_error3/*.input
+inputs_semm_error4=./tests/vstupy/semm_error4/*.input
 i=0 # counter
 
 echo "----------SUCCESS ERROR TESTS 0----------"
 # testy na lexikalni chyby
 for f in $inputs_succ_error0
 do
-    test_name=$(basename $f)
-
+	# PROMENNE
+	filename=$(basename "$f")
+	test_name="${filename%.*}"
 	test_output=$(cat $f | ./$proj) # test output
 	return_value=$? # test result code
 	i=$(($i + 1)) # test number
+    
+	# POKUD BYL VRACEN CODE 0: VYHODNOCENI INTSTRUKCI INTERPRETEREM
+	if [ $return_value -eq 0 ]; then
+		printf "$test_output" > $test_output_file # vypsani ifjcode17 do tmp souboru
+		./$interpreter $test_output_file > $test_output_file 2>&1 # vypsani interpreter output do tmp souboru
+		interpreter_result=$(diff $test_output_file $outputs_succ_error0_path/$test_name.output)
+	fi
+
     # VYPSANI TESTU
-	printf "%-02d  " "$i"
-	if [ "$return_value" -eq "0" ]; then
+	printf "%-02d  " "$i" # vypis cisla testu
+	if [ "$return_value" -eq "0" ] && [ -z "$interpreter_result" ]; then # vypis OK/ERROR
 	    printf "   OK   "
 	else
 	    printf "ERROR   "
 	fi
-	printf "%-8s | EXPECTED=0, RETURNED=$return_value" "$test_name"
-	# POKUD BYL VRACEN CODE 0: VYHODNOCENI INTSTRUKCI INTERPRETEREM
-	if [ $return_value -eq 0 ]; then
-		printf "$test_output" > $test_output_file_path # vypsani ifjcode17 do tmp souboru
-		interpreter_output=$(./$interpreter $test_output_file_path 2>&1) # interpreter output
-		printf "\n$interpreter_output"	
+
+	printf "%s.input | EXPECTED=0, RETURNED=$return_value" "$test_name" # vypis ERROR CODE
+	
+	if [ $return_value -eq 0 ]; then # vypis INTERPRETU
+		if [ -z "$interpreter_result" ]; then
+			printf " INTERPRETER OUTPUT OK"
+		else
+			printf " INTERPRETER OUTPUT DIFFER\n"
+			cat $test_output_file
+		fi
 	fi
 	printf "\n"
 done
